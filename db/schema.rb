750a0e5590d86f5cee9d8ac9a1403da5e3c46b0c
EsterKais <kaisester91@gmail.com>
2016-11-15 16:44:23 +0100

    Messaging system - cannot log in bug

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161115132929) do
+ActiveRecord::Schema.define(version: 20161115143155) do
 
   create_table "book_opinions", force: :cascade do |t|
     t.text     "book_opinion_text"
@@ -37,6 +37,13 @@ ActiveRecord::Schema.define(version: 20161115132929) do
     t.string   "book_link_to_buy"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+  end
+
+  create_table "conversations", force: :cascade do |t|
+    t.integer  "sender_id"
+    t.integer  "recipient_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
   create_table "course_opinions", force: :cascade do |t|
@@ -72,6 +79,17 @@ ActiveRecord::Schema.define(version: 20161115132929) do
     t.integer  "friend_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text     "body"
+    t.integer  "conversation_id"
+    t.integer  "user_id"
+    t.boolean  "read",            default: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "profile_photos", force: :cascade do |t|
