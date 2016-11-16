b4d8261e8abca80d24d44bfeed9ac1804ec7974e
EsterKais <kaisester91@gmail.com>
2016-11-16 10:45:54 +0100

    Home Page Setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'conversations#index'
+  get 'pages/home'
+  root 'pages#home'
   resources :profiles, only: [:new, :edit, :create, :update]
   resources :profilephotos #could be profile_photos
   resources :friendships, only: [:create, :destroy]
