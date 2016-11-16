fa9e81c0e6cef256521ad3cd2996b67935421a3c
EsterKais <kaisester91@gmail.com>
2016-11-16 11:46:27 +0100

    Working/Displaying Retreats Seeds

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
   def home
-
+    @books = Book.all
+    @courses = Course.all
+    @retreats = Retreat.all
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Retreat.delete_all
+# Course.delete_all
+# Book.delete_all
+Profile.delete_all
+User.delete_all
+
+# PROFILES
+ester_profile = Profile.create(first_name: "Ester", last_name: "Kais", years_in_practice: "1 - 5", profile_description: "Lorem Ipsum Yadiyadiyada Ester Profile", teacher: true)
+amber_profile = Profile.create(first_name: "Amber", last_name: "Taal", years_in_practice: "< 1", profile_description: "Lorem Ipsum Yadiyadiyada Amber Profile", teacher: false)
+
+# USERS
+ester = User.create(email: "ester@mindful-living.com", password: "sercret", profile: ester_profile)
+amber = User.create(email: "amber@mindful-living.com", password: "sercret", profile: amber_profile)
+
+# RETREATS
+ester_retreat_one = Retreat.create(retreat_name: "Retreat1", retreat_description: "New yadiyada retreat1 Bla Ester retreat", retreat_link_to_info: "I am a retreat1 link to info", retreat_link_to_booking: "I am link to retreat1 booking", profiles: [ester_profile])
+ester_retreat_two = Retreat.create(retreat_name: "Retreat2", retreat_description: "New yadiyada retreat2 of Now Bla Ester retreat", retreat_link_to_info: "I am a retreat2 link to info", retreat_link_to_booking: "I am link to retreat2 booking", profiles: [ester_profile])
+
+# # COURSES
+# ester_course_one = Course.create(course_name: "Course1", course_description: "New yadiyada Course1 Bla Ester Course", course_link_to_info: "I am a course1 link to info", course_link_to_booking: "I am link to course1 booking", profiles: [ester_profile])
+# ester_course_two = Course.create(course_name: "Course2", course_description: "New yadiyada Course2 of Now Bla Ester Course", course_link_to_info: "I am a course2 link to info", course_link_to_booking: "I am link to course2 booking", profiles: [ester_profile])
+
+# # BOOKS
+# ester_book_one = Book.create(book_name: "The New Earth", book_description: "New yadiyada Earth Bla Ester Book", book_link_to_buy: "I am a earth link to buy", profiles: [ester_profile])
+# ester_book_two = Book.create(book_name: "Power of Now", book_description: "New yadiyada Power of Now Bla Ester Book", book_link_to_buy: "I am a power link to buy", profiles: [ester_profile])
